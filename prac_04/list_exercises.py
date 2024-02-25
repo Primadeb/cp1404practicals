5ead7842244039f0349fc467c187f95e47631bbd
primadebborna <prima.debborna@my.jcu.edu.au>
2024-02-25 22:40:36 +1000

    function to use program to list exercises

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,36 @@
+"""
+CP1404 - prac04
+Data file -> lists program
+"""
+#1
+def main():
+    numbers = []
+
+    for i in range(5):
+        number = float(input("Number: "))
+        numbers.append(number)
+
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+main()
+
+#2
+def main():
+    # List of authorized usernames
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+    # Ask the user for their username
+    user_input = input("Enter your username: ")
+
+    # Check if the entered username is in the list of authorized usernames
+    if user_input in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
+main()
