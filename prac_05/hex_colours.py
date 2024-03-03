34bdf09106fdc6dc7d5b6a5558123bc4def1daa1
primadebborna <prima.debborna@my.jcu.edu.au>
2024-03-03 22:05:21 +1000

    function to use program for hexadecimal color code

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,34 @@
+"""
+Hexadecimal Color Code Lookup
+Estimate: 30 minutes
+Actual:   45 minutes
+"""
+
+# Dictionary containing color names and their hexadecimal codes
+COLOR_CODES = {
+    "AliceBlue": "#F0F8FF",
+    "AntiqueWhite": "#FAEBD7",
+    "Aqua": "#00FFFF",
+    "Aquamarine": "#7FFFD4",
+    "Azure": "#F0FFFF",
+    "Beige": "#F5F5DC",
+    "Bisque": "#FFE4C4",
+    "Black": "#000000",
+    "BlanchedAlmond": "#FFEBCD",
+    "Blue": "#0000FF"
+}
+
+# Displaying available colors
+print("Available Colors:")
+for color in COLOR_CODES:
+    print(color)
+
+# Asking user for input and displaying corresponding hexadecimal code
+while True:
+    color_name = input("Enter color name (or blank to stop): ").capitalize()  # Capitalizing input
+    if color_name == "":
+        break
+    try:
+        print(f"The hexadecimal code for {color_name} is {COLOR_CODES[color_name]}")
+    except KeyError:
+        print("Invalid color name")
