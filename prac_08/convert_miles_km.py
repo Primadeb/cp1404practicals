410069946aac1c856c3f4c9992b67a5fae557bce
primadebborna <prima.debborna@my.jcu.edu.au>
2024-04-05 21:55:38 +1000

    function to use program to get convert miles km

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,24 @@
+"""Estimate time: 3hours
+actual time: 5 hours"""
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
+
+class MilesConverterApp(App):
+    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+    output_text = StringProperty("")
+
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        self.output_text = "Type the number in the field"
+        return self.root
+
+    def handle_calculate(self):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        value = self.get_validated_miles()
+        result = value * MILES_TO_KM
+        self.output_text = str(result)
