88a658d501363236910925190f82b9c575b74495
primadebborna <prima.debborna@my.jcu.edu.au>
2024-09-29 22:08:41 +1000

    Implement temperature conversion with user input for Celsius and Fahrenheit

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -11,3 +11,15 @@ def celsius_to_fahrenheit(celsius):
 def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9 # Formula to convert Fahrenheit to Celsius
 
+#get user input and print output outside the functions
+def main():
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    print(f"{celsius} degrees Celsius is {fahrenheit} degrees Fahrenheit.")
+
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    print(f"{fahrenheit} degrees Fahrenheit is {celsius} degrees Celsius.")
+
+
+main()
