fb1ef02b153d4a1732eb4874edde0dbca1dc437b
primadebborna <prima.debborna@my.jcu.edu.au>
2024-09-29 22:07:28 +1000

    Add functions for temperature conversion between Celsius and Fahrenheit

---
+++ prac_02/temperatures.py
@@ -0,0 +1,13 @@
+"""
+Cp1404 - prac_02
+temperature conversion
+"""
+
+# Function to convert Celsius to Fahrenheit
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9/5 + 32  # Formula to convert Celsius to Fahrenheit
+
+# Function to convert Fahrenheit to Celsius
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9 # Formula to convert Fahrenheit to Celsius
+
