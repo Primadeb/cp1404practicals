9ccda44ddf36bf59cc6dd3448cb8764163e99174
primadebborna <prima.debborna@my.jcu.edu.au>
2024-10-13 18:38:58 +1000

    Add list comprehension examples for processing names and initials

---
+++ prac_04/list_comprehensions.py
@@ -0,0 +1,13 @@
+"""
+CP1404 - prac04
+List comprehensions
+"""
+
+names = ["Bob", "Angel", "Jimi", "Alan", "Ada"]
+full_names = ["Bob Martin", "Angel Harlem", "Jimi Hendrix", "Alan Turing", "Ada Lovelace"]
+
+# for loop that creates a new list containing the first letter of each name
+first_initials = []
+for name in names:
+    first_initials.append(name[0])
+print(first_initials)
