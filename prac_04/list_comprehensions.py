36b28f58adcab36f9da3e6a48230b5e62e19727f
primadebborna <prima.debborna@my.jcu.edu.au>
2024-10-13 19:15:18 +1000

    Refactor loops to list comprehensions for cleaner and more efficient code

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -11,3 +11,42 @@ first_initials = []
 for name in names:
     first_initials.append(name[0])
 print(first_initials)
+
+
+# list comprehension that does the same thing as the loop above
+first_initials = [name[0] for name in names]
+print(first_initials)
+
+# list comprehension that creates a list containing the initials
+# this splits each name and adds the first letters of each part to a string
+full_initials = [name.split()[0][0] + name.split()[1][0] for name in full_names]
+print(full_initials)
+
+# this example uses filtering to select only the names that start with A
+a_names = [name for name in names if name.startswith('A')]
+print(a_names)
+
+# and here's the join string method being used to create a single string from the names like:
+# 'Ada Alan Angel Bob Jimi'
+print(" ".join(sorted(names)))
+
+# list comprehension to create a list of all the full_names in lowercase format
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
+
+almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
+
+# list comprehension to create a list of integers from the above list of strings
+numbers = [int(num) for num in almost_numbers]
+print(numbers)
+
+# list comprehension to create a list of only the numbers that are greater than 9
+filtered_numbers = [num for num in numbers if num > 9]
+print(filtered_numbers)
+
+# use a list comprehension and the join string method to create a string of the last names
+# for those full names longer than 11 characters
+long_last_names = [name.split()[1] for name in full_names if len(name) > 11]
+result_string = ", ".join(long_last_names)
+print(result_string)
+
