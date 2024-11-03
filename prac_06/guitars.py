0e638c92459503dfaa1ec7975f883e52343dd011
primadebborna <prima.debborna@my.jcu.edu.au>
2024-11-03 22:40:02 +1000

    Add main program for creating and displaying Guitar instances

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -13,3 +13,21 @@ def display_guitars(guitars):
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
         print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+def main():
+    print("My guitars!")
+    guitars = []
+
+    # Test data for guitars
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    guitar = get_guitar_details()
+    while guitar is not None:
+        guitars.append(guitar)
+        print(f"{guitar.name} ({guitar.year}) : ${guitar.cost:.2f} added.\n")
+        guitar = get_guitar_details()
+
+    display_guitars(guitars)
+
+main()
