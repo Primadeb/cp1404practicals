f001cb420b5348118dc49d3eefe0cafffdd61533
primadebborna <prima.debborna@my.jcu.edu.au>
2024-11-03 22:27:37 +1000

    Add get_age and is_vintage methods to Guitar class for calculating age and vintage status

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -15,3 +15,12 @@ class Guitar:
     def __str__(self):
         """Return a string representation of the guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return the age of the guitar in years."""
+        current_year = 2022  # Assuming the current year is 2022
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old, False otherwise."""
+        return self.get_age() >= 50
