084b98e0ad9c4747c36f70dc46b8cc6de83f8f57
primadebborna <prima.debborna@my.jcu.edu.au>
2024-11-03 22:26:43 +1000

    Add Guitar class with initialization and string representation methods

---
+++ prac_06/guitar.py
@@ -0,0 +1,17 @@
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar instance.
+
+        name: str, the name of the guitar (default "")
+        year: int, the year the guitar was made (default 0)
+        cost: float, the cost of the guitar (default 0)
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
