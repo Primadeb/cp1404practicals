90e2a9c02a83124da8b0a944a9a56bf8397b559f
primadebborna <prima.debborna@my.jcu.edu.au>
2024-11-25 22:41:14 +1000

    add get_fare and __str__ to the function to run program

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -9,3 +9,11 @@ class SilverServiceTaxi(Taxi):
         """Initialize a SilverServiceTaxi instance, based on parent class Taxi."""
         super().__init__(name, fuel)
         self.price_per_km *= fanciness
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        """Return a string representation of the SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
