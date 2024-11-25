7aef9b58152a75bcb85b02d654966d4d0556fd6e
primadebborna <prima.debborna@my.jcu.edu.au>
2024-11-25 22:45:04 +1000

    add test code

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,21 @@
+
+
+from prac_09.taxi import Taxi
+
+# Create a new taxi object
+my_taxi = Taxi("Prius 1", 100)
+
+# Drive the taxi 40 km
+my_taxi.drive(40)
+
+# Print the taxi's details and the current fare
+print(my_taxi)
+
+# Restart the meter (start a new fare)
+my_taxi.start_fare()
+
+# Drive the car 100 km
+my_taxi.drive(100)
+
+# Print the details and the current fare
+print(my_taxi)
